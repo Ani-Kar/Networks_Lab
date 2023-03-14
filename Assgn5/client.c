c7356342be72d6302c30a1116479ea5ca756f3ec
Aniket Kumar <ani30r.ak@gmail.com>
2023-03-15 04:47:00 +0530

    Started Testing

--- Assgn5/client.c
+++ Assgn5/client.c
@@ -1,6 +1,25 @@
 #include "mysocket.h"
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main()
 {
-    
+    int sockfd = my_socket(AF_INET, SOCK_MyTCP, 0);
+    printf("Socket Created\n");
+
+    struct sockaddr_in Server_Addr;
+    Server_Addr.sin_family = AF_INET;
+    inet_aton("127.0.0.1", &Server_Addr.sin_addr);
+    Server_Addr.sin_port = htons(20000);
+
+    connect(sockfd, (struct sockaddr *)&Server_Addr, sizeof(Server_Addr));
+
+    char *message = "Hello World";
+    my_send(sockfd, message, strlen(message), 0);
+    printf("Message Sent\n");
+    char message_[100];
+    my_recv(sockfd, message_, 100, 0);
+    printf("Message Recieved==>  %s",message_);
+    my_close(sockfd);
 }
---
+++ Assgn5/server.c
@@ -0,0 +1,36 @@
+#include "mysocket.h"
+#include <stdio.h>
+#include <netinet/in.h>
+
+int main()
+{
+    int ServSock,ClientSock;
+    struct sockaddr_in client_addr, server_addr;
+
+    ServSock = my_socket(AF_INET, SOCK_MyTCP, 0);
+    printf("Socket Created\n");
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(20000);
+
+    bind(ServSock, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    printf("Socket Binded\n");
+
+    listen(ServSock, 10);
+    printf("Socket Listening\n");
+
+    int client_len = sizeof(client_addr);
+    ClientSock = accept(ServSock, (struct sockaddr *)& client_addr, &client_len);
+
+    char message[100];
+    my_recv(ClientSock, message, 100, 0);
+    printf("Message Recieved==>  %s",message);
+    char *message_ = "Hello World";
+    my_send(ClientSock, message_, strlen(message_)+1, 0);
+    printf("Message Sent\n");
+    my_close(ClientSock);
+    my_close(ServSock);
+    return 0;
+}
+
